65413246c3078ef82da42c35cc936d38458bb0cb
Melan Jayasinghage <melan@apache.org>
2021-02-15 01:42:03 +0530

    improved API test coverage

--- app/tests/test_apis.py
+++ app/tests/test_apis.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Group, User
 from django.test import Client
 from django.test import TestCase
 
-from app.models import GameRound
+from app.models import GameRound, Team
 
 client = Client()
 auth_headers = {
@@ -27,6 +27,19 @@ class TestAllApis(TestCase):
                                               password='adminadmin')
         admin.user_set.add(admin_user)
 
+        self.colombo_fc_user1 = User.objects.create_user(username='colombo_fc_user1', email='norely@localhost.org',
+                                                         password='adminadmin')
+        player.user_set.add(self.colombo_fc_user1)
+        self.colombo_fc_user2 = User.objects.create_user(username='colombo_fc_user2', email='norely@localhost.org',
+                                                         password='adminadmin')
+        player.user_set.add(self.colombo_fc_user2)
+        self.navy_sc_user1 = User.objects.create_user(username='navy_sc_user1', email='norely@localhost.org',
+                                                      password='adminadmin')
+        admin.user_set.add(self.navy_sc_user1)
+        self.navy_sc_user2 = User.objects.create_user(username='navy_sc_user2', email='norely@localhost.org',
+                                                      password='adminadmin')
+        admin.user_set.add(self.navy_sc_user2)
+
         first_round = GameRound(name='First Round')
         first_round.save()
         quarter_final = GameRound(name='Quarter Final')
@@ -35,11 +48,14 @@ class TestAllApis(TestCase):
         semi_final.save()
         final = GameRound(name='Final')
         final.save()
+        colombo_fc = Team(name='Colombo FC')
+        colombo_fc.save()
+        navy_sc = Team(name='Navy SC')
+        navy_sc.save()
 
     def test_list_all_user(self):
         # get users response
         resp = client.get('/users/', **auth_headers)
-        # self.assertTrue(resp. > 0)
         self.assertEqual(resp.status_code, 200)
 
     def test_create_user(self):
@@ -60,5 +76,48 @@ class TestAllApis(TestCase):
     def test_list_all_groups(self):
         # get API response
         resp = client.get('/groups/', **auth_headers)
-        # self.assertEqual(User.objects.count(), 3)
         self.assertEqual(resp.status_code, 200)
+
+    def test_list_all_teams(self):
+        resp = client.get('/teams/', **auth_headers)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_create_team(self):
+        create_team_payload = {
+            "name": "Oakland Raiders"
+        }
+        resp = client.post('/teams/', json.dumps(create_team_payload),
+                           content_type='application/json', **auth_headers)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_create_game(self):
+        create_game_payload = {
+            "host": "http://127.0.0.1:8000/teams/1/",
+            "guest": "http://127.0.0.1:8000/teams/2/",
+            "winner": "http://127.0.0.1:8000/teams/2/",
+            "date": "2020-12-23",
+            "round": "http://127.0.0.1:8000/rounds/4/"
+        }
+        resp = client.post('/games/', json.dumps(create_game_payload),
+                           content_type='application/json', **auth_headers)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_create_player(self):
+        create_player_payload = {
+            "user": "http://127.0.0.1:8000/users/2/",
+            "team": "http://127.0.0.1:8000/teams/1/",
+            "height": 187
+        }
+        resp = client.post('/players/', json.dumps(create_player_payload),
+                           content_type='application/json', **auth_headers)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_create_coach(self):
+        create_coach_payload = {
+            "user": "http://127.0.0.1:8000/users/3/",
+            "team": "http://127.0.0.1:8000/teams/1/",
+            "height": 187
+        }
+        resp = client.post('/coaches/', json.dumps(create_coach_payload),
+                           content_type='application/json', **auth_headers)
+        self.assertEqual(resp.status_code, 201)
